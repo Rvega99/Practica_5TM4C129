33934419c72c17c0b220f106d85879a7b034d9d3
Rvega99 <112989004+Rvega99@users.noreply.github.com>
2023-01-26 12:53:17 -0600

    Actualizacion no se

--- PWM.c
+++ PWM.c
@@ -1,26 +1,46 @@
 
 #include "lib/include.h"
 
-extern void Configura_Reg_PWM1(uint16_t freq)
+extern void Configura_Reg_PWM1(volatile uint16_t freq)
 {
-    SYSCTL->RCGCPWM |= (1<<0); /*Enable reloj de modulo PWM1 pag 354*/
-    SYSCTL->RCGCGPIO |= (1<<5); /*Enable reloj de GPIO Puerto F pag 340 pin 5*/
-   // GPIOF->AFSEL |= (1<<3)|(1<<2)|(1<<1); /*Control de registros ya sea por GPIO o Otros Pag 672*/
-    GPIOF_AHB->AFSEL |= 0x0E;
+    SYSCTL->RCGCPWM |= (1<<0 ); /*Enable reloj de modulo PWM1 pag 354*/
+    SYSCTL->RCGCGPIO |= (1<<5)|(1<<4); /*Enable reloj de GPIO Puerto F pag 340 pin 5*/
+    GPIOF_AHB->AFSEL |= (1<<3)|(1<<2)|(1<<1); /*Control de registros ya sea por GPIO o Otros Pag 672*/
+    //GPIOF_AHB->AFSEL |= (1<<2);
     GPIOF_AHB->PCTL |= (GPIOF_AHB->PCTL&0xFFFF000F) | 0x00006660; /*Combinado con la tabla Pag 1351 y el registro PCTL le digo que es pwm Pag 689*/
     GPIOF_AHB->DEN |= (1<<3)|(1<<2)|(1<<1); /* para decirle si es digital o no Pag 682*/
-    PWM0->CC &= ~(1<<8);  /*Enable o Disable Divisor  Pag 1747*/
-    PWM0->_0_CTL = (0<<0);
+    PWM0->CC = (1<<8)|(0x3<<0);  /*Enable o Disable Divisor  Pag 1747*/
+    //PWM0->_0_CTL = (0<<0);
     PWM0->_1_CTL = (0<<0); /*Bloqueo y desbloqueo*/
-    PWM0->_1_GENB = 0x0000080C; /*Registro de las acciones del pwm Pag 1285*/
-    PWM0->_1_GENA = 0x0000008C; /*Registro de las acciones del pwm Pag 1282*/
-    PWM0->_0_GENB = 0x0000008C;//PWM5
-    PWM0->_1_LOAD = (int)(20000000/freq); /*cuentas=fclk/fpwm  para 1khz cuentas = (16,000,000/1000)*/
-    PWM0->_0_LOAD = (int)(20000000/freq);
-    PWM0->_1_CMPB = 15000;
-    PWM0->_1_CMPA = 15000;
-    PWM0->_0_CMPA = 15000;
-    PWM0->_1_CTL = (1<<0);// Se activa el generador 3
-    PWM0->_0_CTL = (1<<0);// Se activa el generador 2
-    PWM0->ENABLE = (1<<3) | (1<<2) | (1<<1); /*habilitar el bloque pa que pase Pag 1247*/
+    //CONFIG PWM0 GENERADOR 1A//////////////////////////////////////
+    PWM0->_1_GENA |= (0X2<<2)|(0X3<<6)|(0x0<<0); /*Registro de las acciones del pwm Pag 1282*/
+    PWM0->_1_LOAD = 62500;/*cuentas=fclk/fpwm  para 1khz cuentas = (16,000,000/1000)*/
+    PWM0->_1_CMPB = 2500;
+    PWM0->_1_CMPA = 57813;
+    PWM0->_1_CTL = (1<<0)|(0<<1);
+    //////////////////////////////////////////////////////////////////
+    
+    //CONFIG PWM0 GENERADOR 1B//////////////////////////////////////
+    PWM0->_1_GENB |= (0X2<<2)|(0X3<<10)|(0x0<<0);
+    PWM0->_1_CTL = (1<<0)|(0<<1); 
+   //CONFIG PWM0 GENERADOR 0B//////////////////////////////////////
+    PWM0->_0_CTL = (0<<0);
+    PWM0->_0_GENB |= (0X2<<2)|(0X3<<6)|(0x0<<0);
+    PWM0->_0_LOAD = 62500;/*cuentas=fclk/fpwm  para 1khz cuentas = (16,000,000/1000)*/
+    PWM0->_0_CMPB = 2500;
+    PWM0->_0_CMPA = 57813;
+    PWM0->_0_CTL = (1<<0)|(0<<1);
+    
+    // Se activa el generador 3
+    //PWM0->_0_CTL = (1<<0);// Se activa el generador 2
+    PWM0->ENABLE =  (1<<2)|(1<<1)|(1<<3) ; /*habilitar el bloque pa que pase Pag 1247*/
+}
+ extern void delayms(int i)
+{
+    int a,b;
+    for(a=0;a<i;a++)
+    for(b=0;b<3180;b++)
+    {}
+    
+
 }
--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include "lib/include.h"
-
+int x;
+int i;
 int main(void)
-{
 
-    uint16_t Result[2];
-    uint16_t duty[2];
-    Configurar_PLL();  //Confiuracion de velocidad de reloj
+{
+    volatile uint16_t freq;
+     freq=1250;
+
+    uint16_t Result[3];
+    uint16_t duty[3];
+    Configurar_PLL();  
+    //Confiuracion de velocidad de reloj
     Configura_Reg_ADC0();
     Configurar_UART0();
-    Configura_Reg_PWM1(1000);//Configuro a 1khz el pwm
+    Configura_Reg_PWM1(50);//Configuro a 1khz el pwm
+
+
     while(1)
     {
-        ADC0_InSeq2(Result,duty); //llamada a la conversion por procesador
-        PWM0->_1_CMPB = duty[0];
+         //llamada a la conversion por procesador
+     
+       
+    ADC0_InSeq2(Result,duty);
         PWM0->_1_CMPA = duty[1];
-        PWM0->_0_CMPA = 15000;
-        
-    }
+        delayms(10);
+        PWM0->_1_CMPB = duty[2];
+        delayms(10);
+        PWM0->_0_CMPA = duty[0];          
+      
+       
+}
 
 }
